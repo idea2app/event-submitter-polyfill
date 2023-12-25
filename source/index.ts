cb5ae122b08a38e84577cd2937ff7783ebf101ea
TechQuery <shiy2008@gmail.com>
2023-12-26 07:48:50 +0800

    [fix] 2 compatibility bugs of cross JS runtimes [add] GitHub settings & VS Code extensions [migrate] replace Yarn with PNPM to reduce Disk Usage [optimize] upgrade Tool-chain packages

--- source/index.ts
+++ source/index.ts
@@ -9,41 +9,43 @@ export type ButtonElement = HTMLInputElement | HTMLButtonElement;
 export const SubmitableButton =
     'button, input[type="button"], input[type="submit"], input[type="image"]';
 
-var last_button: ButtonElement | null;
+var last_button: ButtonElement | null | undefined;
 
-document.addEventListener(
-    'click',
-    event =>
-        (last_button = (event.target as Element).closest?.<ButtonElement>(
-            SubmitableButton
-        )),
-    true
-);
+if (typeof document !== 'undefined' && typeof SubmitEvent !== 'function') {
+    document.addEventListener(
+        'click',
+        event =>
+            (last_button = (event.target as Element).closest?.<ButtonElement>(
+                SubmitableButton
+            )),
+        true
+    );
 
-document.addEventListener(
-    'submit',
-    event => {
-        if (last_button && event.submitter) return;
+    document.addEventListener(
+        'submit',
+        event => {
+            if (last_button && event.submitter) return;
 
-        Object.defineProperty(Object.getPrototypeOf(event), 'submitter', {
-            configurable: true,
-            enumerable: true,
-            get(this: SubmitEvent) {
-                const form = this.target as HTMLFormElement,
-                    canditates = [document.activeElement, last_button];
+            Object.defineProperty(Object.getPrototypeOf(event), 'submitter', {
+                configurable: true,
+                enumerable: true,
+                get(this: SubmitEvent) {
+                    const form = this.target as HTMLFormElement,
+                        canditates = [document.activeElement, last_button];
 
-                for (const control of canditates)
-                    if (
-                        control?.matches(SubmitableButton) &&
-                        form === (control as HTMLButtonElement).form
-                    )
-                        return control as ButtonElement;
+                    for (const control of canditates)
+                        if (
+                            control?.matches(SubmitableButton) &&
+                            form === (control as HTMLButtonElement).form
+                        )
+                            return control as ButtonElement;
 
-                return null;
-            }
-        });
-    },
-    true
-);
+                    return null;
+                }
+            });
+        },
+        true
+    );
 
-document.addEventListener('submit', () => (last_button = undefined));
+    document.addEventListener('submit', () => (last_button = undefined));
+}
