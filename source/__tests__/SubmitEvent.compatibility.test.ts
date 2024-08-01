8a5df0c54b1e4135c67db13c58f8a2638345adab
South Drifted <shiy2008@gmail.com>
2024-08-02 07:15:22 +0800

    Update source/__tests__/SubmitEvent.compatibility.test.ts

--- source/__tests__/SubmitEvent.compatibility.test.ts
+++ source/__tests__/SubmitEvent.compatibility.test.ts
@@ -18,7 +18,7 @@ import "../"
 // https://github.com/idea2app/event-submitter-polyfill/issues/8
 it.failing("doesn't polyfill submitter if SubmitEvent is already available", () => {
   const form = document.createElement('form');
-  document.body.appendChild(form);
+  document.body.append(form);
   form.addEventListener("submit", (e) => e.preventDefault())
   form.dispatchEvent(new SubmitEvent("submit"))
   expect(Object.getOwnPropertyDescriptors(SubmitEvent.prototype).submitter.get).toBe(submitterGetter);
